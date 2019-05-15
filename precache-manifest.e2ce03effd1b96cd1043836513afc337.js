101999af1aafe0d4ca78bd01eb3ad0d0e2ddb699
Rocky Nguyen <rockynhatnguyen@gmail.com>
2019-05-15 08:56:30 -0700

    Updates

---
+++ precache-manifest.e2ce03effd1b96cd1043836513afc337.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd2d25edcaca584feff367d51f063a37",
+    "url": "/facedetective/index.html"
+  },
+  {
+    "revision": "f3a384d9e1e2a5ff4397",
+    "url": "/facedetective/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "1cb9fc0dab198429d33a",
+    "url": "/facedetective/static/css/main.1aecdda6.chunk.css"
+  },
+  {
+    "revision": "f3a384d9e1e2a5ff4397",
+    "url": "/facedetective/static/js/2.ce7dd8df.chunk.js"
+  },
+  {
+    "revision": "1cb9fc0dab198429d33a",
+    "url": "/facedetective/static/js/main.48c0f322.chunk.js"
+  },
+  {
+    "revision": "7c6767495e5aa894f8c8",
+    "url": "/facedetective/static/js/runtime~main.ead05b95.js"
+  },
+  {
+    "revision": "e009a67dbabdc4d3e3bd1bbae03a7aeb",
+    "url": "/facedetective/static/media/logo.e009a67d.png"
+  }
+]);
